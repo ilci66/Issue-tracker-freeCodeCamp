98fb475faa0b9de93834c0db0e91e748d0d5a918
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-05-31 01:03:52 +0200

    can't get the GET route working yet
    there is always an empty array whenever I try to filter somehow, tried many different approaches, nothing so far worked

--- models.js
+++ models.js
@@ -3,13 +3,13 @@ const { Schema } = mongoose;
 
 
 const issueSchema = new Schema({
+  assigned_to: String,
+  open: { type: Boolean, default: true },
   issue_title: {type: String, required: true},
   issue_text: {type: String, required: true},
   created_on: Date,
   updated_on: Date,
   created_by: {type: String, required: true},
-  assigned_to: String,
-  open: { type: Boolean, default: true },
   // open: Boolean,
   status_text: String
 });
--- routes/api.js
+++ routes/api.js
@@ -13,8 +13,7 @@ module.exports = function (app) {
 //you need to be able to filter /api/issues/{project}?open=false
 //with any given field and value, 
 
-//make an array include the search criteria if it exists      
-
+//make an array include the search criteria if it exists  
       let { project } = req.params;
       //console.log(req.params.open)
       const {
@@ -24,37 +23,66 @@ module.exports = function (app) {
         issue_text,
         created_by,
         assigned_to,
-        status_text
+        status_text,
       } = req.query;
 
+      
+      Project.findOne({name:project}, (err, data) => {
+        // res.send((data.issues))
+        // res.send(data.issues)
+ //boolean value of open somehow messes it up, even though it returns 
+ //the given query result, somehow doesn't work for neither query nor 
+ //filtering after getting the data
+        console.log(open)
+        let foundIssues = data.issues;
+        // console.log(foundIssues)
+        let responseArray = [];
+
+        foundIssues.map(ele => {
+          console.log(open)
+          if(open && ele.open == true){
+            responseArray.push(ele)
+          }if(!open && ele.open == false){
+            negativeRes.push(ele)
+          }
+        })
+        console.log(open,"here")
+        if(open){res.json(responseArray)}
+        else if(!open){res.json("false")}
+
+      })
+
+
+//saw the exact code work in another video, the exact query
+//when I add open=true (or any query really) returns an empty array
       // let queryToMatch = [{$match: {name: project}},{$unwind:"$issues"}];
 
       // if(_id != undefined){
-      //   queryToMatch.push({"_id": _id})
+      //   queryToMatch.push({$match: {"_id": _id}})
       // }
       // if(open != undefined){
       //   console.log(open, "its")
-      //   queryToMatch.push({"open": open})
+      //   queryToMatch.push({$match: {"open": open}})
       // }
       // if(issue_title != undefined){
-      //   queryToMatch.push({"issue_title": issue_title})
+      //   queryToMatch.push({$match:{"issue_title": issue_title}})
       // }
       // if(issue_text != undefined){
-      //   queryToMatch.push({"issue_text": issue_text})
+      //   queryToMatch.push({$match:{"issue_text": issue_text}})
       // }
       // if(created_by != undefined){
-      //   queryToMatch.push({"created_by": created_by})
+      //   queryToMatch.push({$match:{"created_by": created_by}})
       // }
       // if(assigned_to != undefined){
-      //   queryToMatch.push({"assigned_to": assigned_to})
+      //   queryToMatch.push({$match:{"assigned_to": assigned_to}})
       // }
       // if(status_text != undefined){
-      //   queryToMatch.push({"status_text": status_text})
+      //   queryToMatch.push({$match:{"status_text": status_text}})
       // }
       // console.log(queryToMatch)
       // res.json("works")
 
-      // Project.aggregate([$match:queryToMatch], (err, data) => {
+      // Project.aggregate(queryToMatch, (err, data) => {
       //   //console.log(data)
       //   res.json(data)
       // })
@@ -64,17 +92,47 @@ module.exports = function (app) {
         // res.json(data.issues.map(ele => ele))
       // })
 
+  //     saw this one on youtube too, 
+  //     ran into the same issue 
+  //     if(open){console.log("open")}
+
+  //     Project.aggregate([
+  //       {$match: { name: project } },
+  //       // {$unwind: "$issues" },
+  //       _id != undefined
+  //       ? {$match :{"issues._id":ObjectId(_id)}}
+  //       : {$match: {} },
+  //       open !=undefined
+  //       ? {$match: {"issues.open": open}} 
+  //       : {$match: {} },
+  //       issue_title != undefined
+  //       ? {$match: {"issues.issue_title": issue_title}}
+  //       : {$match: {} },
+  //       created_by != undefined
+  //       ? {$match: {"issues.created_by": created_by}}
+  //       : {$match: {} },
+  //       assigned_to != undefined
+  //       ? {$match: {"issues.assigned_to": assigned_to}}
+  //       : {$match: {} },
+  //       status_text != undefined
+  //       ? {$match: {"issues.status_text": status_text}}
+  //       : {$match: {} },
+  //     ]).exec((err, data) => {
+  //       console.log(data[0].issues)
+  //         let mappedData = []
+  //         data[0].issues.map(ele => mappedData.push(ele))
+  //         res.json(mappedData);
+  //     })
       
     })
-    
     .post(function (req, res){
       let project = req.params.project;
       const {
+        assigned_to,
+        status_text,
         issue_title,
         issue_text,
         created_by,
-        assigned_to,
-        status_text
       } = req.body;
 // if(issue_title==""||issue_text==""||created_by=="")
       if(!issue_title||!issue_text||!created_by){
@@ -83,14 +141,14 @@ module.exports = function (app) {
 
     //create an issue, don't forget to add dates(create and update)
       let newIssue = new Issue({
+        assigned_to: assigned_to || "",
+        status_text: status_text || "",
+        open:true,
         issue_title: issue_title || "",
         issue_text: issue_text || "",
-        created_on: new Date(),
-        updated_on: new Date(),
         created_by: created_by || "",
-        assigned_to: assigned_to || "",
-        open:true,
-        status_text: status_text || "" 
+        created_on: new Date(),
+        updated_on: new Date()
       })
 
     //find the project in your db, if there isn't one make one
@@ -125,12 +183,13 @@ module.exports = function (app) {
     
     .put(function (req, res){
       let project = req.params.project;
+      res.json("put")
       
     })
     
     .delete(function (req, res){
       let project = req.params.project;
-      
+      res.json("delete")
     });
     
 };
