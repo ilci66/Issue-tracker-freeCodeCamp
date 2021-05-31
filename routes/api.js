f453f53950d3766641382fd2c2bbafc22a0a9032
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-05-31 17:15:30 +0200

    GET tests are still failing but It works
    there is a weird issue with booleans when using $match, couldn't fix it even asked people on stack overflow, nothing

--- routes/api.js
+++ routes/api.js
@@ -9,14 +9,9 @@ module.exports = function (app) {
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-//all the issues with this url /api/issues/{projectname}
-//you need to be able to filter /api/issues/{project}?open=false
-//with any given field and value, 
-
-//make an array include the search criteria if it exists  
       let { project } = req.params;
       //console.log(req.params.open)
-      const {
+      let {
         _id,
         open,
         issue_title,
@@ -25,197 +20,40 @@ module.exports = function (app) {
         assigned_to,
         status_text,
       } = req.query;
+      let foundIssues = []
 
-//boolean value of open somehow messes it up, even though it returns 
-//the given query result, somehow doesn't work for neither query nor 
-//filtering after getting the data
-//for loop looked awful
-      Project.findOne({name:project}, (err, data) => {
-        let foundIssues = data.issues;
-        let responseArray = [];
-        let arrayForFiltering = [];
-
-        if(open != undefined){
-          if(responseArray.length == 0){
-            foundIssues.map(ele => {
-              // console.log(ele.open.toString() == open.toString())
-              if(ele.open.toString() == open.toString()){
-                responseArray.push(ele)
-              }
-            })
-          }else if(responseArray.length != 0){
-            responseArray.map(ele => {
-              if(ele.open.toString() == open.toString()){
-                arrayForFiltering.push(ele)
-              }
-            })
-            responseArray = arrayForFiltering;
+      if(open != undefined || _id != undefined|| issue_title != undefined|| issue_text != undefined|| created_by != undefined|| assigned_to != undefined|| status_text != undefined){
+        Project.findOne({name: project}, (err, data) => {
+          foundIssues = data.issues;
+          if(open != undefined){
+            foundIssues = foundIssues.filter(ele => open.toString() == ele.open.toString())
           }
-        }
-
-        if(_id != undefined){
-          if(responseArray.length == 0){
-            foundIssues.map(ele => {
-              if(ele._id == issue_tit_idle){
-                responseArray.push(ele)
-              }
-            })
-          }else if(responseArray.length != 0){
-            responseArray.map(ele => {
-              if(ele._id == _id){
-                arrayForFiltering.push(ele)
-              }
-            })
-            responseArray = arrayForFiltering;
+          if(_id != undefined){
+            foundIssues = foundIssues.filter(ele => _id == ele._id)
           }
-        }
-
-        if(issue_title != undefined){
-          if(responseArray.length == 0){
-            foundIssues.map(ele => {
-              if(ele.issue_title == issue_title){
-                responseArray.push(ele)
-              }
-            })
-          }else if(responseArray.length != 0){
-            responseArray.map(ele => {
-              if(ele.issue_title == issue_title){
-                arrayForFiltering.push(ele)
-              }
-            })
-            responseArray = arrayForFiltering;
+          if(issue_title != undefined){
+            foundIssues = foundIssues.filter(ele => issue_title == ele.issue_title)
           }
-        }
-
-        if(issue_text != undefined){
-          if(responseArray.length == 0){
-            foundIssues.map(ele => {
-              if(ele.issue_text == issue_text){
-                responseArray.push(ele)
-              }
-            })
-          }else if(responseArray.length != 0){
-            responseArray.map(ele => {
-              if(ele.issue_text == issue_text){
-                arrayForFiltering.push(ele)
-              }
-            })
-            responseArray = arrayForFiltering;
+          if(issue_text != undefined){
+            foundIssues = foundIssues.filter(ele => issue_text == ele.issue_text)
           }
-        }
-
-        if(created_by != undefined){
-          if(responseArray.length == 0){
-            foundIssues.map(ele => {
-              if(ele.created_by == created_by){
-                responseArray.push(ele)
-              }
-            })
-          }else if(responseArray.length != 0){
-            responseArray.map(ele => {
-              if(ele.created_by == created_by){
-                arrayForFiltering.push(ele)
-              }
-            })
-            responseArray = arrayForFiltering;
+          if(created_by != undefined){
+            foundIssues = foundIssues.filter(ele => created_by == ele.created_by)
           }
-        }
-        if(assigned_to != undefined){
-          if(responseArray.length == 0){
-            foundIssues.map(ele => {
-              if(ele.assigned_to == assigned_to){
-                responseArray.push(ele)
-              }
-            })
-          }else if(responseArray.length != 0){
-            responseArray.map(ele => {
-              if(ele.assigned_to == assigned_to){
-                arrayForFiltering.push(ele)
-              }
-            })
-            responseArray = arrayForFiltering;
+          if(assigned_to != undefined){
+            foundIssues = foundIssues.filter(ele => assigned_to == ele.assigned_to)
           }
-        }
-
-        if(status_text != undefined){
-          if(responseArray.length == 0){
-            foundIssues.map(ele => {
-              if(ele.status_text == status_text){
-                responseArray.push(ele)
-              }
-            })
-          }else if(responseArray.length != 0){
-            responseArray.map(ele => {
-              if(ele.status_text == status_text){
-                arrayForFiltering.push(ele)
-              }
-            })
-            responseArray = arrayForFiltering;
+          if(status_text != undefined){
+            foundIssues = foundIssues.filter(ele => status_text == ele.status_text)
           }
-        }
-        let tempArray = []
-        if(open != undefined || _id != undefined || issue_title != undefined ||issue_text != undefined || created_by != undefined || assigned_to != undefined || status_text != undefined){
-          
-          for(let i = 0; i < responseArray.length; i++){
-            if(tempArray.indexOf(responseArray[i]) < 0){
-              tempArray.push(responseArray[i])
-            }
-          }
-          responseArray = tempArray
-          res.json(responseArray)
-        } else {
+        res.json(foundIssues)
+        })
+      }else{
+        Project.findOne({name: project}, (err, data) => {
           res.json(data.issues)
-        }
-      })
-
-
-//saw the exact code work in another video, the exact query
-//when I add open=true (or any query really) returns an empty array
-
-        
-      // if(open != undefined) {
-      //   open = open.toString();
-      // }
-      // let queryToMatch = [{$match: {name: project}},{$unwind:"$issues"}];
-
-      // if(_id != undefined){
-      //   queryToMatch.push({$match: {"_id": _id}})
-      // }
-      // if(open != undefined){
-      //   queryToMatch.push({$match: {"open": open}})
-      // }
-      // if(issue_title != undefined){
-      //   queryToMatch.push({$match:{"issue_title": issue_title}})
-      // }
-      // if(issue_text != undefined){
-      //   queryToMatch.push({$match:{"issue_text": issue_text}})
-      // }
-      // if(created_by != undefined){
-      //   queryToMatch.push({$match:{"created_by": created_by}})
-      // }
-      // if(assigned_to != undefined){
-      //   queryToMatch.push({$match:{"assigned_to": assigned_to}})
-      // }
-      // if(status_text != undefined){
-      //   queryToMatch.push({$match:{"status_text": status_text}})
-      // }
-      // console.log(queryToMatch)
-      // // res.json("works")
-
-      // Project.aggregate(queryToMatch, (err, data) => {
-      //   //console.log(data)
-      //   res.json(data)
-      // })
-
-      // Project.findOne({name: project}, (err, data) => {
-        //console.log(data)
-        // res.json(data.issues.map(ele => ele))
-      // })
-
-  //     saw this one on youtube too, 
-  //     ran into the same issue 
-  //     if(open){console.log("open")}
-      
+        })
+      }
+//open doesn't work when it's a boolean, no idea why, tried many different approaches
       // Project.aggregate([
       //   {$match: { name: project } },
       //   {$unwind: "$issues" },
@@ -223,7 +61,7 @@ module.exports = function (app) {
       //   ? {$match :{"issues._id":ObjectId(_id)}}
       //   : {$match: {} },
       //   open !=undefined
-      //   ? {$match: {"issues.open": open}} 
+      //   ? {$match: {"issues.open"$eq:{open}}} 
       //   : {$match: {} },
       //   issue_title != undefined
       //   ? {$match: {"issues.issue_title": issue_title}}
@@ -240,11 +78,11 @@ module.exports = function (app) {
       // ]).exec((err, data) => {
       //   console.log(data.status_text)
       //     let mappedData = []
-      //     // data.issues.map(ele => mappedData.push(ele))
-      //     res.json(data);
+      //     data.issues.map(ele => mappedData.push(ele))
+      //     res.json(mappedData);
       // })
-      
     })
+
     .post(function (req, res){
       let project = req.params.project;
       const {
@@ -303,7 +141,18 @@ module.exports = function (app) {
     
     .put(function (req, res){
       let project = req.params.project;
-      res.json("put")
+      let {
+        _id,
+        open,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+      } = req.body;
+      console.log(open)
+      res.json("this be put")
+      //find the issue by id inside given project first 
       
     })
     
